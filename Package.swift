b351622bdbfb0ce35346f68d814313eb81a6b98e
Jadavananda Bordoloi <jadavananda@acquire.io>
2026-09-08 15:27:10 +0530

    -ObjC unsafe flag added

--- Package.swift
+++ Package.swift
@@ -19,6 +19,9 @@ let package = Package(
                 "AcquireIO",
                 "AcquireIOSockets",
                 "WebRTC"
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-ObjC"])
             ]
         ),
         .testTarget(
